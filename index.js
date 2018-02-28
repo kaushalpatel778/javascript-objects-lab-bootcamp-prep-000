597c784adba5f8ec771ab5430ced0d23f45dd42a
Kaushal Patel <kaushalpatel778@gmail.com>
2018-02-28 04:02:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var recipes = {};
+<<<<<<< HEAD
 //var obj = {prop: 1};
 function updateObjectWithKeyAndValue (object, key, value) {
 
   return Object.assign({}, object, {[key] : value});
+=======
+var obj = {prop: 1};
+function updateObjectWithKeyAndValue (object, key, value) {
+
+  return Object.assign({}, obj, {[key] : value});
+>>>>>>> e54a4ca8679931ba0aa0297feb829db482d0aec0
 
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
@@ -10,6 +17,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
+<<<<<<< HEAD
 function deleteFromObjectByKey(object, key) {
   var clone = Object.assign({}, object);
   delete clone[key];
@@ -18,4 +26,13 @@ function deleteFromObjectByKey(object, key) {
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
+=======
+function deleteFromObjectByKey(object, key, value) {
+  delete object.key;
+  return obj;
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+>>>>>>> e54a4ca8679931ba0aa0297feb829db482d0aec0
 }
